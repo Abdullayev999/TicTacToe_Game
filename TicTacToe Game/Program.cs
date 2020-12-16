eb4053774abba354e89af139e2c9713043f2358c
Farid <fabdullayev902@gmail.com>
2020-12-16 15:51:09 +0400

    first commit

---
+++ TicTacToe Game/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace TicTacToe_Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TicTacToe tmp = new TicTacToe();
+            Console.WriteLine(tmp);
+            int count = 0;
+            char symbl;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine(tmp);
+
+                if (count % 2 == 0)
+                {
+                    Console.Write("symbl : X");
+                    symbl = 'X';
+                }
+                else
+                {
+                    Console.Write("symbl : Y");
+                    symbl = 'Y';
+                }
+
+                Console.Write("\nEner num (0-9) : ");
+                int.TryParse(Console.ReadLine(), out int num);
+
+                if (count % 2 == 0)
+                {
+                    if (tmp.MakeMove(num, symbl))
+                    {
+                        count++;
+                        if (tmp.ChekWin('X')) break;
+                    }
+                    else Console.WriteLine("Number entered incorrectly");
+                }
+                else
+                {
+                    if (tmp.MakeMove(num, symbl))
+                    {
+                        count++;
+                        if (tmp.ChekWin('Y')) break;
+                    }
+                    else Console.WriteLine("Number entered incorrectly");
+
+                }
+            } while (true);
+
+            Console.Clear();
+            Console.WriteLine(tmp);
+            Console.WriteLine($"{symbl} is win!!!");
+        }
+    }
+}
---
+++ TicTacToe Game/TicTacToe.cs
@@ -0,0 +1,70 @@
+﻿namespace TicTacToe_Game
+{
+    class TicTacToe
+    {
+        private char[] Board { get; set; }
+        private const int Count = 9;
+        public TicTacToe()
+        {
+            Board = new char[Count] { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', };
+        }
+        public bool ChekWin(char symbl)
+        {
+            for (int i = 0; i < 7; i += 3)// check horizontally
+            {
+                if (Board[i] == symbl && Board[i + 1] == symbl && Board[i + 2] == symbl)
+                {
+                    return true;
+                }
+            }
+
+            for (int i = 0; i < 3; i++)//check vertically
+            {
+                if (Board[i] == symbl && Board[i + 3] == symbl && Board[i + 6] == symbl)
+                {
+                    return true;
+                }
+            }
+
+            if ((Board[0] == symbl && Board[4] == symbl && Board[8] == symbl) || //check obliquely
+               (Board[2] == symbl && Board[4] == symbl && Board[6] == symbl))
+            {
+                return true;
+            }
+            return false;
+        }
+        public bool MakeMove(int x, char symbl)
+        {
+            if (x >= 0 && x <= 8)
+            {
+                if (Board[x] == ' ')
+                {
+                    Board[x] = symbl;
+                    return true;
+                }
+            }
+            return false;
+        }
+        public override string ToString()
+        {
+            string str = "";
+            for (int i = 0; i < Count; i++)
+            {
+                str += "| ";
+                if (Board[i] != ' ')
+                    str += Board[i];
+                else
+                {
+                    str += " ";
+                }
+                str += " |";
+
+                if ((i + 1) % 3 == 0)
+                {
+                    str += "\n";
+                }
+            }
+            return str;
+        }
+    }
+}
